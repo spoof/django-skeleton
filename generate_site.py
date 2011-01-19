ae379f8f24e51ddb853995d80e872fabf81010d4
Sergey Safonov <spoof@spoofa.info>
2011-01-20 02:34:03 +0300

    Refactoring of config reading. Added gunicorn.conf, supervisord.confg

--- generate_site.py
+++ generate_site.py
@@ -8,65 +8,55 @@ Copyright (c) 2011 . All rights reserved.
 """
 
 import os, sys
-import re
+import re, copy
 import yaml
 
+
+AVAILABLE_SECTIONS = ['settings', 'directories', 'files', 'copy']
 RE_VALID_PROJECT_NAME = re.compile('\w+$')
 RE_VALID_PORT = re.compile('\d+$')
 
 CONFIG_DIR = 'conf'
 
-def check_file_exist(filename, paths=[]):
-    """Check ether or not file exist in `paths` or in $PATH variable"""
-    if not paths:
-        paths = os.environ["PATH"].split(os.pathsep)
-    return any([os.path.exists(os.path.join(p, filename)) for p in paths])
+def normalize_to_dict(data, context={}, update_context=False):
+    result = {}
+    def _update_result(k, v, context=context, update_context=update_context):
+        r = context and v % context or v
+        if update_context:
+            if not context.has_key(k):
+                context[k] = r
+        result[k] = r
+    
+    if isinstance(data, list):
+        for value in data:
+            for k, v in value.items():
+                _update_result(k, v)
+    
+    elif isinstance(data, dict):
+        for k, v in data.items():
+            _update_result(k, v)
+    return result
+    
+def get_variable(settings, var, description, error='Cannot be empty', test_function=lambda x: x, empty=False):
+    if settings.has_key(var):
+        return settings[var]
+    
+    while 1:
+        var = raw_input(description + ': ')
+        if not empty:
+            if not test_function(var):
+                sys.stderr.write("Error: %s\n" % error)
+                continue
+            else:
+                break
+        else:
+            break
+    return var
 
 def initialize(config_file='conf.yaml'):
-    config = {
-        'base_site': os.getcwd()
-    }
-    while 1:
-        input_msg = 'Project name'
-        project_name = raw_input(input_msg + ': ')
-        if not RE_VALID_PROJECT_NAME.match(project_name):
-            sys.stderr.write("Error: That project name is invalid. It must not contain spaces.\n")
-            continue
-        
-        if project_name:
-            config['project_name'] = project_name
-            break
-    while 1:
-        input_msg = 'Site name'
-        site_name = raw_input(input_msg + ': ')
-        if site_name:
-            config['site_name'] = site_name
-            break
-    while 1:
-        input_msg = 'HTTP/FastCGI port'
-        port = raw_input(input_msg + ': ')
-        if not RE_VALID_PORT.match(port):
-            sys.stderr.write("Error: That port is invalid. Use only digits.\n")
-            continue
-        
-        if port:
-            config['port'] = port
-            break
-    input_msg = 'Project directory (Leave blank to use current directory)'
-    base_site = raw_input(input_msg + ': ')
-    if base_site:
-        base_site = os.path.expanduser(base_site)
-        try:
-            if not os.path.exists(base_site):
-                os.makedirs(base_site)
-        except OSError, e:
-            print "Error: ", e
-    
-    site_root = os.path.join(config['base_site'], project_name)
     current_dir = os.path.dirname(os.path.realpath(__file__))
     config_file = os.path.join(current_dir, config_file)
-    configs_dir = os.path.join(current_dir, "conf")
-    project_root = os.path.join(site_root, project_name)
+    configs_dir = os.path.join(current_dir, CONFIG_DIR)
     
     try:
         f = open(config_file, 'r')
@@ -75,37 +65,75 @@ def initialize(config_file='conf.yaml'):
     except OSError, e:
         sys.stderr.write("Couldn't read config file. %s \n" % (str(e)))
     
-    config['settings'].update({
+    settings = normalize_to_dict(config['settings'])
+    project_name = get_variable(settings, 'project_name', description='Project name', 
+                                 error='That project name is invalid. It must not contain spaces.', 
+                                 test_function=lambda x: RE_VALID_PROJECT_NAME.match(x))
+    site_name = get_variable(settings, 'site_name', description='Site name')
+    port = get_variable(settings, 'port', description='HTTP/FastCGI port', 
+                                 error='That port is invalid. Use only digits', 
+                                 test_function=lambda x: RE_VALID_PORT.match(x))
+    base_site = get_variable(settings, 'base_site', description='Project directory (Leave blank to use current directory)',  
+                                 empty=True)
+    
+    if base_site:
+        base_site = os.path.expanduser(base_site)
+        try:
+            if not os.path.exists(base_site):
+                os.makedirs(base_site)
+        except OSError, e:
+            print "Error: ", e
+    else:
+        base_site = os.getcwd()
+    
+    production_root = get_variable(settings, 'production_root', description='Production site root', error='Production site root cannot be empty')
+    production_root = production_root.rstrip("/")
+    site_root = os.path.join(base_site, project_name)
+    project_root = os.path.join(site_root, project_name)
+    
+    config['context'] = {
         'site_root': site_root,
         'project_name': project_name,
         'site_name': site_name,
         'port': port,
-        'base_site': base_site or os.getcwd(),
+        'base_site': base_site,
         'configs_dir': configs_dir,
         'project_root': project_root,
-    })
-    print config
+        'production_root': os.path.join(production_root, project_name),
+    }
+    
+    for section_name in AVAILABLE_SECTIONS:
+        try:
+            section = config[section_name]
+        except KeyError:
+            continue
+        
+        update_context = section_name in ('settings', 'directories')
+        section_normalized = normalize_to_dict(section, context=config['context'], update_context=update_context)
+        if not update_context:
+            config[section_name].update(section_normalized)
+    
     return config
 
 def generate():
     try:
         config = initialize()
-        settings = config['settings']
+        context = config['context']
         directories = config['directories']
         files = config['files']
         print 'Creating a virtualenv for the project'
-        os.system('cd %(base_site)s && virtualenv --no-site-packages %(project_name)s' % settings)
-        os.chdir(settings['site_root'])
-        os.system('pip -E . install -r %(configs_dir)s/dev_requirements.pip && pip -E . install -r %(configs_dir)s/requirements.pip' % settings)
-        print 'Creating the Django project \'%(project_name)s\'' % (settings)
-        os.system('source bin/activate && django-admin.py startproject %(project_name)s' % settings)
+        os.system('cd %(base_site)s && virtualenv --no-site-packages %(project_name)s' % context)
+        os.chdir(context['site_root'])
+        os.system('pip -E . install -r %(configs_dir)s/dev_requirements.pip && pip -E . install -r %(configs_dir)s/requirements.pip' % context)
+        print 'Creating the Django project \'%(project_name)s\'' % (context)
+        os.system('source bin/activate && django-admin.py startproject %(project_name)s' % context)
         
         if directories:
             print "Creating directory stucture"
         for directory_dict in directories:
             for k, v in directory_dict.items():
                 try:
-                    os.makedirs(v % settings)
+                    os.makedirs(v % context)
                 except OSError, e:
                     if e.errno != 17:
                         raise OSError(e)
@@ -116,10 +144,10 @@ def generate():
         from django.template.loader import get_template
         
         django_settings.configure(
-                TEMPLATE_DIRS=(settings['configs_dir'],),
+                TEMPLATE_DIRS=(context['configs_dir'],),
             )
         
-        context = Context(settings)
+        context = Context(context)
         def render_template(template_name, dest, context):
             content = get_template(template_name).render(context)
             f = open(dest % context, 'w')
